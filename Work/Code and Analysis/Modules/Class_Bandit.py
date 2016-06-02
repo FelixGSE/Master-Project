3054799f04db02ab2e53b2fff99ff36c8d85934a
Dom <fizlaz@gmail.com>
2016-06-02 21:24:55 +0200

    d05

--- Work/Code and Analysis/Modules/Class_Bandit.py
+++ Work/Code and Analysis/Modules/Class_Bandit.py
@@ -4,9 +4,13 @@ class bandit:
     SET INITIAL CONDITIONS
     """
     
-    def __init__( self, mu = [0,0] , sigma = [1,1], N = 10 , seed = None ):
+    def __init__( self, mu = [0,0] , sigma = [1,1], N = 10 , seed = None, iowa=False,\
+                p1=0.5,p2=0.9,p3=0.5,p4=0.9,s1=[100,-150],s2=[100,-1150],s3=[50,0],s4=[50,-200]):
         self.mu = mu
-        self.sigma = sigma
+        if iowa == False:
+            self.sigma = sigma
+        else:
+            self.sigma = ["no","sigma"]
         self.N = N
         self.seed = seed
         if self.seed == None:
@@ -19,13 +23,20 @@ class bandit:
     Function to create bandits
     """
     
-    def create_bandits(self, mu ,sigma, N ):
-        n_bandit = len( mu )
-        bandits = []
-        for i in range( n_bandit ):
-            number = self.rnorm( mu[i] , sigma[i] , N )
-            bandits.append(number)
-        return bandits
+    def create_bandits(self, mu ,sigma, N, iowa, p1,p2,p3,p4,s1,s2,s3,s4 ):
+        if iowa == False:
+            n_bandit = len( mu )
+            bandits = []
+            for i in range( n_bandit ):
+                number = self.rnorm( mu[i] , sigma[i] , N )
+                bandits.append(number)
+            return bandits
+        else:
+            n_bandit = 4
+            bandits = []
+            for i in range(n_bandit):
+                number = weighted_sample()
+
     
     """ 
     Auxilliary functions
